3801282e4f1c0a16a5e4f0f15ad73ad5523beebd
dclarkstew <47606490+dclarkstew@users.noreply.github.com>
2024-05-10 18:48:04 -0600

    Update read_temp_data.py

--- read_temp_data.py
+++ read_temp_data.py
@@ -2,6 +2,7 @@ import datetime as dt
 import matplotlib.pyplot as plt
 import matplotlib.ticker as tck
 from scipy import integrate
+import numpy as np
 # import plotly.express as px
 # from datetime import datetime
 
@@ -92,16 +93,26 @@ def read_temp_data(filename):
 
 
     # --- Temp compare ---
+    temp_saved_list = []
     power_saved_list = []
     money_saved_list = []
     money_saved_dict = {}
     for key,comare_data in data_dict.items():
         if 'front' in key and 'control' not in key:
-            output1, recovered_kwh = money_saved(data_dict['ambient'],comare_data,data_dict['seconds'])
+            output1, recovered_kwh, recovered_temp = money_saved(data_dict['ambient'],comare_data,data_dict['seconds'])
             money_saved_dict[key] = round(output1,3)
             money_saved_list.append(round(output1,2))
             power_saved_list.append(round(recovered_kwh,2))
-    money_saved_list.sort()
+            temp_saved_list.append(round(recovered_temp,2))
+
+    
+    # --- Sort lists ascending ---
+    list_reorder = list(np.argsort(money_saved_list))
+    list_reorder.reverse()
+
+    money_saved_list = [x for _,x in sorted(zip(list_reorder,money_saved_list))]
+    power_saved_list = [x for _,x in sorted(zip(list_reorder,power_saved_list))]
+    temp_saved_list = [x for _,x in sorted(zip(list_reorder,temp_saved_list))]
 
     # === Plot ===
     # fig, ax1 = plt.subplots()
@@ -131,38 +142,66 @@ def read_temp_data(filename):
 
     # === Plot ===
     fig, ax2  = plt.subplots()
+
+    # --- Font ---
+    SMALL_SIZE = 8
+    MEDIUM_SIZE = 10
+    BIGGER_SIZE = 12
+
+    plt.rc('font', size=12)          # controls default text sizes
+    plt.rc('axes', titlesize=40)     # fontsize of the axes title
+    plt.rc('axes', labelsize=40)    # fontsize of the x and y labels
+    plt.rc('xtick', labelsize=40)    # fontsize of the tick labels
+    plt.rc('ytick', labelsize=40)    # fontsize of the tick labels
+    plt.rc('legend', fontsize=40)    # legend fontsize
+    plt.rc('figure', titlesize=20)  # fontsize of the figure title
     
-    bar_legend_name = ['red', 'blue', 'red', 'orange']
-    bar_colors = ['tab:red', 'tab:blue', 'tab:red', 'tab:orange']
+    bar_legend_name = ['red', 'blue', 'green', 'orange']
+    bar_colors = ['tab:red', 'tab:blue', 'tab:green', 'tab:orange']
 
     money_saved_items = []
     for key,value in money_saved_dict.items():
+        if key == 'nyc_front':
+            key = '1/2" x 1/2" x 3" bars'
+        if key == 'venice_front':
+            key = '1/8" x 3" fins'
+        if key == 'wichita_front':
+            key = '1/8" flat plate'
+        if key == 'behive_front':
+            key = '1/4" Honeycomb\nGrid Core'
         money_saved_items.append(key)
-    bars = ax2.bar(money_saved_items, money_saved_list, label=bar_legend_name, color=bar_colors)
+    money_saved_items = [x for _,x in sorted(zip(list_reorder,money_saved_items))]
 
-    ax2.set_ylabel('% gained')
+    bars = ax2.bar(money_saved_items, money_saved_list, color=bar_colors, label=money_saved_items)
+
+    ax2.set_ylabel('% gained', fontsize=20, fontweight='bold')
+    
+    # --- Figure Title ---
     time_start = data_dict['time'][0]
     time_end = data_dict['time'][-1]
+    ax2.set_title('Percent efficiency gained by design\n('+ str(time_start) + ' to ' + str(time_end) + ')',fontsize=28,fontweight='bold') 
 
-    ax2.set_title('Percent efficiency gained by design ('+ str(time_start) + ' to ' + str(time_end) + ')') #zzz have it say over
-    ax2.legend(title='Design Name')
+    # ax2.legend(title='Design')
 
     ax2.grid(visible=True, which='major', axis='y',color='green', linestyle='-', linewidth = 1.5)
     ax2.grid(visible=True, which='minor', axis='y',color='lime', linestyle='--')
     ax2.set_axisbelow(True)
-    ax2.tick_params(axis='y', which='minor', bottom=False)
-    
-    # bars = ax2.barh(indexes, values)
-    # ax2.bar_label(bars)
-    for i, v in enumerate(money_saved_list):
-        ax2.text(i-.40, v + .075, str(v)+'% / '+str(power_saved_list[i])+' kWh', color='black', fontweight='bold', verticalalignment='center')
-        # zzz also need to add average temperature saved. Use trapezoid method
-    
 
+    ax2.tick_params(axis='y', labelsize=20)
     ax2.yaxis.set_minor_locator(tck.AutoMinorLocator())
 
-    plt.show()
+    ax2.tick_params(axis='x', labelsize=20)
+    plt.xticks(money_saved_items, weight = 'bold')
+
+    # --- Add column values ---
+    for i, v in enumerate(money_saved_list):
+        ax2.text(i-.38, v-.4, str(v)+'%\n' + str(power_saved_list[i]) + ' kWh\n' + str(temp_saved_list[i]) + ' °C', 
+                 color='black', 
+                #  fontweight='bold', 
+                 verticalalignment='center',
+                 fontsize=24)
     
+    plt.show()
     g=0
 
 # === Sub functions =====
@@ -179,6 +218,10 @@ def money_saved(ambient_data,compare_data,seconds_data):
     theoretical_power_inte = integrate.trapezoid(theoretical_power_loss,seconds_data)
     experimental_power_inte = integrate.trapezoid(experimental_power_loss,seconds_data)
 
+    theoretical_temp_inte = integrate.trapezoid(ambient_data,seconds_data) / seconds_data[-1]
+    experimental_temp_inte = integrate.trapezoid(compare_data,seconds_data) / seconds_data[-1]
+    recovered_temp = theoretical_temp_inte - experimental_temp_inte
+
     tot_power_diff = experimental_power_inte - theoretical_power_inte
     in_kWh = tot_power_diff/1000/3600
 
@@ -189,7 +232,7 @@ def money_saved(ambient_data,compare_data,seconds_data):
     in_dollars = in_kWh*price_per_kWh*200*5
     b = theoretical_power_inte/1000/3600*200*5
     
-    return recovered_percent, recovered_kwh
+    return recovered_percent, recovered_kwh, recovered_temp
     # return in_dollars
 
 
