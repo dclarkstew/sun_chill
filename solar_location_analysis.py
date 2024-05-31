2fe5513b0aa083e7e0e635052d2f9006e9575bbe
dclarkstew <47606490+dclarkstew@users.noreply.github.com>
2024-05-30 23:07:03 -0600

    Update solar_location_analysis.py
    Broke out trapezoidal summation into days, and other code clean up

--- solar_location_analysis.py
+++ solar_location_analysis.py
@@ -7,17 +7,17 @@ from datetime import datetime
 from scipy import integrate
 
 # Local imports
-import read_temp_data as read_temp_data
+import read_temp_data as read_temp_data # type: ignore
 # from heat_sink_design import heat_sink_design
  
 def main():
     # reading the CSV file
     nrel_directory = r'C:\Users\dclar\Documents\Solar Panels\analysis\theoretical\\'
-    NREL_files = {'denver_1947888_39.74_-105.03_2022_localTime.csv',
+    NREL_files = ['denver_1947888_39.74_-105.03_2022_localTime.csv',
                 'phoenix_1309015_33.44_-112.05_2022_localTime.csv',
                 'atlanta_3894477_33.80_-84.39_2022_localTime.csv',
                 'sahara_487671_22.77_5.54_2019_localTime.csv',         # Tamanrasset
-                'brussels_454991_50.85_4.34_2019_localTime.csv'}
+                'brussels_454991_50.85_4.34_2019_localTime.csv']
 
     # === Solar Panel Inputs ===
     # https://es-media-prod.s3.amazonaws.com/media/components/panels/spec-sheets/HyundaiEnergySolutions_HiA-SXXXHG_M2_300_315W_xSe5w2v.pdf
@@ -27,17 +27,12 @@ def main():
     SP_power_rating = 305 # Watts
     SP_efficiency = 0.184 # %
     eff_loss_per_degree = 0.0306 # %
-
     price_per_kWh = 0.16 # dollars
-
-    dissapation_delta = 8.45 # celcius
-    num_bars = 10
 
     # --- Constants ---
     # Specific heat capacity (J/g-C)
     c_silicon = 0.705 # Silicon
     c_alum = 0.897 # Aluminium
-
 
     # Heat transfer coefficient (W/m-K)
     k_silicon = 0.2 # Silicon
@@ -63,33 +58,28 @@ def main():
     # ========================
 
     nrel_dict = []
-    nrel_in_sun_dict = []
-
     for idx_file,i_csv in enumerate(NREL_files):
 
+        # --- Initialize ---
         NREL_datetime = []
         NREL_DNI =[]
         NREL_temp = []
-
         nrel_temp_in_sun = []
         nrel_time_in_sun = []
         nrel_dni_in_sun = []
-        power_loss_due_to_temp = 0
         i_power_ideal = []
         i_power_heated = []
-
-        flag_found_sunrise_idx = False
-        sunrise_idx = []
 
         filename_split = i_csv.split('_')
         filename_location = filename_split[0]
 
-        # if filename_location == 'phoenix':
-        #     phx_idx = idx_file
+        nrel_dict.append( { 'name': filename_location,'data_days':[] } )
 
         nrel_filepath = nrel_directory + i_csv
         with open(nrel_filepath, newline='') as csvNREL:
             NREL_reader = csv.reader(csvNREL, delimiter=' ', quotechar='|')
+
+            day_last = 1
             for row_idx,row in enumerate(NREL_reader):
 
                 # Start of data
@@ -101,10 +91,20 @@ def main():
                     day = int(split_row[2])
                     hour = int(split_row[3])
                     minute = int(split_row[4])
-                    # NREL_datetime.append(datetime(year, month, day, hour, minute))
 
-                    # NREL_DNI.append(int(split_row[5]))
-                    # NREL_temp.append(float(split_row[6])) # Celcius
+                    # --- Is the entry on the same day? ---
+                    if day_last != day:
+                        nrel_dict[-1]['data_days'].append( {'time': nrel_time_in_sun,
+                                                            'dni' : NREL_DNI,
+                                                            'temp': nrel_temp_in_sun,
+                                                            'power_ideal': i_power_ideal,
+                                                            'power_heated':i_power_heated } )
+                        day_last = day
+                        nrel_time_in_sun = []
+                        NREL_DNI = []
+                        nrel_temp_in_sun = []
+                        i_power_ideal = []
+                        i_power_heated = []     
 
                     # --- Sun has risen ---
                     if float(split_row[5]) > 0:
@@ -112,12 +112,7 @@ def main():
                         i_dni = int(split_row[5])
                         nrel_temp_in_sun.append(i_temp)
                         nrel_time_in_sun.append(datetime(year, month, day, hour, minute))
-                        nrel_dni_in_sun.append(i_dni)
-
-                        # --- Find sun rise ---
-                        if flag_found_sunrise_idx == False:
-                            sunrise_idx = row_idx
-                            sunrise_dt = datetime(year, month, day, hour, minute)                        
+                        nrel_dni_in_sun.append(i_dni)                        
 
                         # --- Pretend this is the temp of the panel not the air ---
                         temp_above_roomtemp = i_temp - room_temp
@@ -135,134 +130,89 @@ def main():
                             i_power_ideal.append(i_power_possible)
                             i_power_heated.append(i_power_possible - (i_power_possible * efficiency_loss))
 
-            # nrel_dict.append( { 'name': filename_location,
-            #                     'time': NREL_datetime,
-            #                     'dni' : NREL_DNI,
-            #                     'temp': NREL_temp} )
-            
-            nrel_dict.append( { 'name': filename_location,
-                                'time': nrel_time_in_sun,
-                                'dni' : NREL_DNI,
-                                'temp': nrel_temp_in_sun,
-                                'power_ideal': i_power_ideal,
-                                'power_heated':i_power_heated } )
+            # --- Need this to get the last day ---
+            nrel_dict[-1]['data_days'].append( {'time': nrel_time_in_sun,
+                                                'dni' : NREL_DNI,
+                                                'temp': nrel_temp_in_sun,
+                                                'power_ideal': i_power_ideal,
+                                                'power_heated':i_power_heated } )
             
     # === Sum data for power loss from temperature ===
-    power_sum_ideal_kwh = {}
-    heatsink_power_sum = {}
-    money_sum_ideal = {}
-    heatsink_money_sum = {}
-    for i_loc in nrel_dict:
+    power_sum_dict = {}
+    money_sum_dict = {}
 
-        key = i_loc['name']
+    # --- Loop through each location ---
+    for i_location_data in nrel_dict:
 
-        start_time = i_loc['time'][0]
-        sec_from_start = [(start_time - val).seconds for val in i_loc['time']]
+        key = i_location_data['name']
+        
+        # --- Initialize sums ---
+        power_sum_dict[key] = {}
+        money_sum_dict[key] = {}
+        power_sum_dict[key]['ambient'] = 0
+        money_sum_dict[key]['ambient'] = 0
+        for i_heatsink in ordered_design_names:
+            power_sum_dict[key][i_heatsink] = 0
+            money_sum_dict[key][i_heatsink] = 0 
+        
+        # --- Loop through each day at each location ---
+        for i_day in i_location_data['data_days']:
 
-        # --- Integration needs to be broken up for day to day --- 
-        integrated_kwh = integrate.trapezoid(i_loc['power_ideal'],sec_from_start) /1000/3600
-        power_sum_ideal_kwh[key] = integrated_kwh
-        money_sum_ideal[key] = round(integrated_kwh * price_per_kWh,2)
+            if i_day['time'] == []:
+                continue
 
-        heatsink_power_sum[key] = {}
-        heatsink_money_sum[key] = {}
-        for idx,i_heatsink in enumerate(ordered_design_names):
-            heatsink_scaled_power_kwh = integrated_kwh * (1+percent_saved_list[idx]/100)
+            start_time = i_day['time'][0]
 
-            heatsink_power_sum[key][i_heatsink] = heatsink_scaled_power_kwh
-            heatsink_money_sum[key][i_heatsink] = round(heatsink_scaled_power_kwh * price_per_kWh,2)
-    gg= 0
-    plot2_wtf(money_sum_ideal,heatsink_money_sum)
+            sec_from_start = [(val - start_time).seconds for val in i_day['time']]
+
+            # --- Integrate power over day and sum money earned for ambient temperatures ---
+            integrated_kwh = integrate.trapezoid(i_day['power_heated'],x=sec_from_start) /1000/3600
+            power_sum_dict[key]['ambient'] += integrated_kwh
+            money_sum_dict[key]['ambient'] += integrated_kwh * price_per_kWh
+
+            # --- Do the same but included heat sinks ---
+            for idx,i_heatsink in enumerate(ordered_design_names):
+                heatsink_scaled_power_kwh = integrated_kwh * (1+percent_saved_list[idx]/100)
+
+                power_sum_dict[key][i_heatsink] += heatsink_scaled_power_kwh
+                money_sum_dict[key][i_heatsink] += heatsink_scaled_power_kwh * price_per_kWh
+
+    # --- Rearrange dict ---
+    heatsink_type_location_dict = {key:{k:money_sum_dict[k][key] for k in money_sum_dict if key in money_sum_dict[k]} for key in ordered_design_names}
+
+    plot2_wtf(money_sum_dict)
     gsd=0
 
-def plot2_wtf(money_sum_ideal,heatsink_money_sum):
+def plot2_wtf(money_sum_dict):
 
-    fig, ax  = plt.subplots()
+    # fig, ax = plt.subplots(1,5)
+    fig, ax = plt.subplots()
 
-    # for key,value in heatsink_build_costs.items():
+    plot_dict = {}
 
-    list_to_plot = []
-    names = []
-    kk=0
-    for key,value in money_sum_ideal.items():
+    for key,i_location_data in money_sum_dict.items():
 
-        ambient_generation = value
-        list_to_plot.append(ambient_generation)
-        names.append(str(ambient_generation))
+        plot_dict[key] = {}
+        plot_dict[key]['x_vals'] = []
+        plot_dict[key]['y_vals'] = []
+        for key2,i_design_value in i_location_data.items():
 
-        hf = heatsink_money_sum[key]
-        for key2,value2 in heatsink_money_sum[key].items():
-            heatsink_generation = heatsink_money_sum[key][key2]
-            list_to_plot.append(heatsink_generation)
-            names.append(str(heatsink_generation))
+            val_rounded = round(i_design_value,2)
 
-            g=0
+            plot_dict[key]['x_vals'].append(str(val_rounded))
+            plot_dict[key]['y_vals'].append( val_rounded )
 
-        list_to_plot.append(0)
-        
-        kk += 1
-        names.append(str(kk))
+    # --- Plot per location ---
+    for i_loc in ['brussels','atlanta','denver','sahara','phoenix']:
 
-    bars = ax.bar(names, list_to_plot)
+        x_vals = plot_dict[i_loc]['x_vals']
+        y_vals = plot_dict[i_loc]['y_vals']
+
+        ax.bar(x_vals,y_vals)
+
     plt.show()
 
     d=0    
-
-
-    # --- Ideal ---
-    # calc_power_sum_ideal = 0
-    # for idx,this_power in enumerate(i_loc['power_ideal']):
-
-    #     if idx == len(i_loc['power_ideal'])-1:
-    #         # print('break')
-    #         break
-
-    #     this_time = i_loc['time'][idx]
-    #     next_time = i_loc['time'][idx+1]
-    #     time_delta = (next_time - this_time).total_seconds() / 3600
-    #     next_power = i_loc['power_ideal'][idx+1]
-
-    #     trapezoid = ((this_power + next_power) / 2) * time_delta
-    #     calc_power_sum_ideal += trapezoid
-
-    # --- Heated ---
-    # calc_power_sum_heated = 0
-    # for idx,this_power in enumerate(i_loc['power_heated']):
-
-    #     if idx == len(i_loc['power_heated'])-1:
-    #         # print('break')
-    #         break
-
-    #     this_time = i_loc['time'][idx]
-    #     next_time = i_loc['time'][idx+1]
-    #     time_delta = (next_time - this_time).total_seconds() / 3600
-    #     next_power = i_loc['power_heated'][idx+1]
-
-    #     trapezoid = ((this_power + next_power) / 2) * time_delta
-    #     calc_power_sum_heated += trapezoid
-
-    # # money_per_year_ideal = calc_power_sum_ideal/1000 * price_per_kWh
-    # money_per_year_heated = calc_power_sum_heated/1000 * price_per_kWh
-
-    # kWhr_saved = (hs_power_sum - calc_power_sum)/1000 # kW-hr
-
-    # money_lost = money_per_year_ideal - money_per_year_heated
-
-    # percent_lost = money_lost / money_per_year_ideal * 100
-
-    # nrel_dict.append( {
-    #     'ideal gen':  money_per_year_ideal,
-    #     'actual gen': money_per_year_heated,
-    #     'money lost': money_lost,
-    #     'percent lost': percent_lost
-    # })
-
-    # print('*'*50)
-    # print(i_loc['name'])
-    # print('Ideal generation: $',round(money_per_year_ideal,2))
-    # print('Actual generation: $',round(money_per_year_heated,2))
-    # print('Money lost $',round(money_lost,2))
-    # print('% lost:',round(percent_lost,2),'%\n')
 
 
 # === Plot ===
